e17c7f1088a0d422b06e30d0dccb33f955f30ba1
Denis Bulaev <bulaev.qa@gmail.com>
2023-03-02 18:24:15 +0300

    commit

--- src/test/java/test/CvConstructorTest.java
+++ src/test/java/test/CvConstructorTest.java
@@ -1,47 +1,46 @@
-package test;
-
-import faker.UserData;
-import org.junit.jupiter.api.Tag;
-import steps.ResumeSteps;
-import steps.AuthPrr;
-import settings.PrrUrl;
-import owner.OwnerData;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-public class CvConstructorTest {
-
-    PrrUrl prrUrl = new PrrUrl();
-    AuthPrr authPrr = new AuthPrr();
-    ResumeSteps createCv = new ResumeSteps();
-    UserData userData = new UserData();
-    OwnerData ownerData = new OwnerData();
-
-    @Test
-    @Tag("cvTest")
-    @DisplayName("Тест создание резюме")
-    public void cvConstructorTest() {
-        prrUrl.openBrowser();
-        authPrr.clickLogIn()
-                .clickESIA()
-                .setLogin(ownerData.loginCv)
-                .setPassword(ownerData.passCv)
-                .clickCV();
-        createCv.buttonCloseCookie()
-                .buttonCV()
-                .positionName()
-                .clickPhoto()
-                .importPhoto()
-                .uploadPhoto()
-                .clickSphere()
-                .randomSphere()
-                .salary(userData.salary)
-                .clickRegion()
-                .region()
-                .favoriteMail()
-                .lvlWork()
-                .buttonSaveResume()
-                .pageMyResume()
-                .urlCheck();
-    }
-}
+//package test;
+//
+//import faker.UserData;
+//import org.junit.jupiter.api.Tag;
+//import steps.ResumeSteps;
+//import steps.AuthPrr;
+//import settings.PrrUrl;
+//import owner.OwnerData;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//
+//public class CvConstructorTest {
+//
+//    PrrUrl prrUrl = new PrrUrl();
+//    AuthPrr authPrr = new AuthPrr();
+//    ResumeSteps createCv = new ResumeSteps();
+//    UserData userData = new UserData();
+//    OwnerData ownerData = new OwnerData();
+//
+//    @Test
+//    @Tag("cvTest")
+//    @DisplayName("Тест создание резюме")
+//    public void cvConstructorTest() {
+//        prrUrl.openBrowser();
+//        authPrr.clickLogIn()
+//                .clickESIA()
+//                .setLogin(ownerData.loginCv)
+//                .setPassword(ownerData.passCv)
+//                .clickCV();
+//        createCv.buttonCloseCookie()
+//                .buttonCV()
+//                .positionName()
+//                .clickPhoto()
+//                .importPhoto()
+//                .uploadPhoto()
+//                .clickSphere()
+//                .randomSphere()
+//                .salary(userData.salary)
+//                .clickRegion()
+//                .region()
+//                .favoriteMail()
+//                .lvlWork()
+//                .buttonSaveResume()
+//                .pageMyResume();
+//    }
+//}
--- src/test/java/test/NavigationTest.java
+++ src/test/java/test/NavigationTest.java
@@ -1,28 +1,28 @@
-package test;
-
-import owner.OwnerData;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import settings.PrrUrl;
-import steps.AuthPrr;
-import steps.ResumeSteps;
-
-public class NavigationTest {
-    PrrUrl prrUrl = new PrrUrl();
-    AuthPrr authPRR = new AuthPrr();
-    ResumeSteps createCV = new ResumeSteps();
-    OwnerData ownerData = new OwnerData();
-    @Test
-    @DisplayName("Тест навигации по резюме")
-    public void cvConstructorTest() {
-        prrUrl.openBrowser(); // Авторизация
-        authPRR.clickLogIn()
-                .clickESIA()
-                .setLogin(ownerData.loginCv)
-                .setPassword(ownerData.passCv)
-                .clickCV();
-        createCV.buttonCloseCookie() // Создание резюме
-                .buttonCV()
-                .navigation();
-    }
-}
+//package test;
+//
+//import owner.OwnerData;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import settings.PrrUrl;
+//import steps.AuthPrr;
+//import steps.ResumeSteps;
+//
+//public class NavigationTest {
+//    PrrUrl prrUrl = new PrrUrl();
+//    AuthPrr authPRR = new AuthPrr();
+//    ResumeSteps createCV = new ResumeSteps();
+//    OwnerData ownerData = new OwnerData();
+//    @Test
+//    @DisplayName("Тест навигации по резюме")
+//    public void cvConstructorTest() {
+//        prrUrl.openBrowser(); // Авторизация
+//        authPRR.clickLogIn()
+//                .clickESIA()
+//                .setLogin(ownerData.loginCv)
+//                .setPassword(ownerData.passCv)
+//                .clickCV();
+//        createCV.buttonCloseCookie() // Создание резюме
+//                .buttonCV()
+//                .navigation();
+//    }
+//}
