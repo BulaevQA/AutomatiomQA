eb976cdcd86659747828175962318b0d2700c503
Denis Bulaev <bulaev.qa@gmail.com>
2023-03-05 22:18:57 +0300

    oop tests fix varables

--- src/test/java/helpers/TestValues.java
+++ src/test/java/helpers/TestValues.java
@@ -4,15 +4,11 @@ import pages.MainCvPage;
 
 public class TestValues {
 
-//    public static String URL = System.getProperty("base_url", "http://localhost:8080/");
-//    public static String LOGIN = System.getProperty("login", "admin");
-//    public static String PASSWORD = System.getProperty("password", "admin");
+    public static String URL = System.getProperty("base_url", "http://localhost:8080/");
+    public static String LOGIN = System.getProperty("login", "admin");
+    public static String PASSWORD = System.getProperty("password", "admin");
     public static String POSITION_NAME = "Автотест";
     public static String SALARY = "35000";
-    public static String URL = "https://web-hotfix-test02.k8s.trudvsem.ru";
-    public static String LOGIN = "89674407691";
-    public static String PASSWORD = "Ex3t8yh96mj!";
     public static String EXPECTED_CABINET = "Мой кабинет";
     public static String EXPECTED_MY_RESUME = "Мои резюме";
-
 }
