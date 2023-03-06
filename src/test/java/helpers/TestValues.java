e647a30efb845ec6365739d2e0d224102e19ffef
Denis Bulaev <bulaev.qa@gmail.com>
2023-03-06 23:05:15 +0300

    test merge

--- src/test/java/helpers/TestValues.java
+++ src/test/java/helpers/TestValues.java
@@ -4,14 +4,11 @@ import com.github.javafaker.Faker;
 
 public class TestValues {
 
-//    public static String URL = System.getProperty("base_url", "http://localhost:8080/");
-//    public static String LOGIN = System.getProperty("login", "admin");
-//    public static String PASSWORD = System.getProperty("password", "admin");
+    public static String URL = System.getProperty("base_url", "http://localhost:8080/");
+    public static String LOGIN = System.getProperty("login", "admin");
+    public static String PASSWORD = System.getProperty("password", "admin");
     public static String POSITION_NAME = new Faker().bothify("Автотест####");
     public static String SALARY = "35000";
     public static String EXPECTED_CABINET = "Мой кабинет";
     public static String EXPECTED_MY_RESUME = "Мои резюме";
-    public static String URL = "https://rtportal.show.pbs.bftcom.com/";
-    public static String LOGIN = "89674407691";
-    public static String PASSWORD = "Ex3t8yh96mj!";
 }
